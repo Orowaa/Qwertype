b811116250c8db7e9a453c62c319c090f9500733
SergeBoucka <serge.boucka@gmail.com>
2024-03-21 16:18:14 +0100

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = [
+  "Pas de panique !",
+  "La vie l’univers, et le reste",
+  "Merci pour le poisson",
+];
---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+function afficherResultat(score, nombreMotsProposes) {
+  console.log("Votre score est de " + score + " / " + nombreMotsProposes);
+}
+
+function choisirPhrasesOuMots() {
+  let choix = prompt(
+    'Choississez une liste "mots" ou "phrases" pour choisir le type de jeu'
+  );
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt(
+      'Choississez une liste "mots" ou "phrases" pour choisir le type de jeu'
+    );
+  }
+  return choix;
+}
+
+function lancerBoucleDeJeu(listePropositions) {
+  let score = 0;
+  for (let i = 0; i < listePropositions.length; i++) {
+    motUtilisateur = prompt("Recopiez : " + listePropositions[i]);
+    if (motUtilisateur === listePropositions[i]) {
+      score++;
+    }
+  }
+  return score;
+}
+
+function lancerJeu() {
+  let choix = choisirPhrasesOuMots();
+  let score = 0;
+  let nombreMotsProposes = 0;
+
+  if (choix === "mots") {
+    console.log("Vous avez choisi le jeu des mots");
+    score = lancerBoucleDeJeu(listeMots);
+    nombreMotsProposes = listeMots.length;
+  } else {
+    choix = "phrases";
+    console.log("Vous avez choisi le jeu des phrases");
+    score = lancerBoucleDeJeu(listePhrases);
+    nombreMotsProposes = listePhrases.length;
+  }
+  afficherResultat(score, nombreMotsProposes);
+}
+
