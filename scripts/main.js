b897be874007845e2c6d5ebcad6248bd8627987e
SergeBoucka <serge.boucka@gmail.com>
2024-04-29 17:34:17 +0200

    presque_end

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,26 @@ lancerJeu();
 
 // id de la div contenant la zone de saisie
 let inputEcriture = document.getElementById("inputEcriture");
-console.log(inputEcriture);
+//console.log(inputEcriture);
 
 // id du bouton de validation
 let btnValiderMot = document.getElementById("btnValiderMot");
-console.log(btnValiderMot);
+//console.log(btnValiderMot);
 
 // Div contenant la zone de proposition il n a pas d id donc on utilise querySelector
 // pour selectionner la classe de la div on ecrit un point devant le nom de la classe
 let divZoneproposition = document.querySelector(".zoneProposition");
-console.log(divZoneproposition);
+//console.log(divZoneproposition);
 
 
 // pour selectionner span de score on utilise querySelector 
 // pour selectionner la classe de la div on ecrit un point devant le nom de la classe
 // et pour selectionner le span on ecrit un espace et le nom de la balise span
 let spanScore = document.querySelector(".zoneScore span");
-console.log(spanScore);
+//console.log(spanScore);
 
 // pour selectionner tous les boutons radio on utilise querySelectorAll
 // pour selectionner la classe de la div on ecrit un point devant le nom de la classe
 // et pour selectionner les input on ecrit un espace et le nom de la balise input
 let listeBtnRadio = document.querySelectorAll(".optionSource input");
-console.log(listeBtnRadio);
+//console.log(listeBtnRadio);
--- scripts/script.js
+++ scripts/script.js
@@ -29,58 +29,112 @@ function afficherProposition(proposition) {
  * @param {string} score : le score.
  */
 function afficherEmail(nom, email, score) {
-  let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`;
+  let mailto = `mailto:${email}?subject=Partage du score Quertype&body=Salut, je suis ${nom} et je viens de réaliser un score de ${score} sur le site de Quertype ! Essaye de faire mieux !`;
   location.href = mailto;
 }
 
 /**
- * Cette fonction lance le jeu.
- * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+ *
+ * @param {string} nom
+ * @throws {Error} si le nom est trop court
  */
-function lancerJeu() {
-  // Initialisations
-  initAddEventListenerPopup();
-  let score = 0;
-  let i = 0;
-  let listeProposition = listeMots;
+function validerNom(nom) {
+  if (nom.length < 2) {
+    throw new Error("Le nom est trop court");
+  }
+}
 
-  let btnValiderMot = document.getElementById("btnValiderMot");
-  let inputEcriture = document.getElementById("inputEcriture");
+function validerEmail(email) {
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+  );
+  if (!emailRegExp.test(email)) {
+    throw new Error("L'email n'est pas valide");
+  }
+}
+function afficherMessageErreur(message) {
+  let spanErreurMessage = document.getElementById("erreurMessage");
 
-  afficherProposition(listeProposition[i]);
+  if (!spanErreurMessage) {
+    let popup = document.querySelector("popup");
+    let spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
 
-  // Gestion de l'événement click sur le bouton "valider"
-  btnValiderMot.addEventListener("click", () => {
-    if (inputEcriture.value === listeProposition[i]) {
-      score++;
-    }
-    i++;
-    afficherResultat(score, i);
-    inputEcriture.value = "";
-    if (listeProposition[i] === undefined) {
-      afficherProposition("Le jeu est fini");
-      btnValiderMot.disabled = true;
-    } else {
-      afficherProposition(listeProposition[i]);
-    }
-  });
+    popup.append(spanErreurMessage);
+  }
+  spanErreurMessage.innerText = message;
+}
 
-  // Gestion de l'événement change sur les boutons radios.
-  let listeBtnRadio = document.querySelectorAll(".optionSource input");
-  for (let index = 0; index < listeBtnRadio.length; index++) {
-    listeBtnRadio[index].addEventListener("change", (event) => {
-      // Si c'est le premier élément qui a été modifié, alors nous voulons
-      // jouer avec la listeMots.
-      if (event.target.value === "1") {
-        listeProposition = listeMots;
-      } else {
-        // Sinon nous voulons jouer avec la liste des phrases
-        listeProposition = listePhrases;
-      }
-      // Et on modifie l'affichage en direct.
-      afficherProposition(listeProposition[i]);
-    });
+function gererFormulaire(scoreEmail) {
+  try {
+    let baliseNom = document.getElementById("nom");
+    let nom = baliseNom.value;
+    validerNom(nom);
+
+    let balisEmail = document.getElementById("email");
+    let email = balisEmail.value;
+    validerEmail(email);
+    afficherMessageErreur("");
+    afficherEmail(nom, email, scoreEmail);
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message);
   }
 
-  afficherResultat(score, i);
+  /**
+   * Cette fonction lance le jeu.
+   * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+   */
+  function lancerJeu() {
+    // Initialisations
+    initAddEventListenerPopup();
+    let score = 0;
+    let i = 0;
+    let listeProposition = listeMots;
+
+    let btnValiderMot = document.getElementById("btnValiderMot");
+    let inputEcriture = document.getElementById("inputEcriture");
+
+    afficherProposition(listeProposition[i]);
+
+    // Gestion de l'événement click sur le bouton "valider"
+    btnValiderMot.addEventListener("click", () => {
+      if (inputEcriture.value === listeProposition[i]) {
+        score++;
+      }
+      i++;
+      afficherResultat(score, i);
+      inputEcriture.value = "";
+      if (listeProposition[i] === undefined) {
+        afficherProposition("Le jeu est fini");
+        btnValiderMot.disabled = true;
+      } else {
+        afficherProposition(listeProposition[i]);
+      }
+    });
+
+    // Gestion de l'événement change sur les boutons radios.
+    let listeBtnRadio = document.querySelectorAll(".optionSource input");
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+      listeBtnRadio[index].addEventListener("change", (event) => {
+        // Si c'est le premier élément qui a été modifié, alors nous voulons
+        // jouer avec la listeMots.
+        if (event.target.value === "1") {
+          listeProposition = listeMots;
+        } else {
+          // Sinon nous voulons jouer avec la liste des phrases
+          listeProposition = listePhrases;
+        }
+        // Et on modifie l'affichage en direct.
+        afficherProposition(listeProposition[i]);
+      });
+    }
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let scoreEmail = `${score} / ${i}`;
+      gererFormulaire(scoreEmail);
+    });
+
+    afficherResultat(score, i);
+  }
 }
